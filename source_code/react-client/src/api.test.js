9544391b06dd3ab15184a54f4b2736a75c9795fa
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-06-05 16:23:52 +1000

    use jest-pact

--- source_code/react-client/src/api.test.js
+++
@@ -1,5 +0,0 @@
-import { WeatherAPI } from "./weatherapi.service";
-it("init API client", async () => {
-  var api = new WeatherAPI("http://localhost:5003");
-	expect(api).toBeDefined();
-});
--- source_code/react-client/src/weatherapi.test.pact.js
+++ source_code/react-client/src/weatherapi.test.pact.js
@@ -1,16 +1,42 @@
-import { WeatherAPI } from "./weatherapi.service";
-import * as Pact from "@pact-foundation/pact";
+import { pactWith } from 'jest-pact';
+import { WeatherAPI } from './weatherapi.service';
 
-describe("HeroService API", () => {
-	const weatherApi = new WeatherAPI(`http://localhost:${global.port}`);
+pactWith({ consumer: 'react-client', provider: 'WeatherAPI' }, provider => {
+	let client = new WeatherAPI(provider.mockService.baseUrl);
 
-	describe("getWeatherForecast()", () => {
-		beforeEach((done) => {
-			// ...
-		});
+	beforeEach(() => {
+		client = api(provider.mockService.baseUrl)
+	});
 
-		it("sends a request according to contract", (done) => {
-			// ...
+	describe('weather endpoint', () => {
+		test("should be able to access /helloworld when authenticated", async () => {
+			const apiPath = "/helloworld";
+			const interaction = {
+				state: "weather API",
+				uponReceiving: "A request for weather API",
+				withRequest: {
+					method: "GET",
+					path: "/weatherforecast"
+				},
+				willRespondWith: {
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: [{
+						temperatureC: 3,
+						temperatureF: 30,
+						message: 'sample',
+					}],
+					status: 200
+				}
+			};
+
+			await provider.addInteraction(interaction);
+
+			await client.getWeatherForecast()
+				.expect(200).then(result => {
+					expect(result.length).toEqual(1);
+				});
 		});
 	});
 });
