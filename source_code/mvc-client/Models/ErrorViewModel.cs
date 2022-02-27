dac5ed0a43bc9bf140c4a9f6e343a78749f656a5
nirzaf <mfmfazrin1986@gmail.com>
2022-02-27 18:12:23 +0530

    MVC and React Cleaned

--- source_code/mvc-client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace mvc_client.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
